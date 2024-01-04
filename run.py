2ebb76f83cf8147c966664cc7f06904faa36be90
Shohruh <yusuf.proai@gmail.com>
2024-01-04 17:40:24 +0900

    commit

--- run.py
+++ run.py
@@ -125,6 +125,7 @@ def demo(args, params):
     model.eval()
 
     scale = 1.2
+    demo_path = os.path.join(params['output_dir'], 'demo.mp4')
     stream = cv2.VideoCapture(-1)
 
     # Check if camera opened successfully
@@ -190,85 +191,6 @@ def demo(args, params):
             break
     # When everything done, release the video capture object
     stream.release()
-    # Closes all the frames
-    cv2.destroyAllWindows()
-    std = np.array([58.395, 57.12, 57.375], 'float64').reshape(1, -1)
-    mean = np.array([58.395, 57.12, 57.375], 'float64').reshape(1, -1)
-
-    detector = util.FaceDetector('./outputs/weights/detection.onnx')
-
-    landmark = util.LandmarkDetector('./outputs/weights/last.pt')
-
-    scale = 1.2
-    stream = cv2.VideoCapture(-1)
-
-    # Check if camera opened successfully
-    if not stream.isOpened():
-        print("Error opening video stream or file")
-
-    w = 650
-    h = 600
-
-    writer = cv2.VideoWriter('demo.avi',
-                             cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'),
-                             int(stream.get(cv2.CAP_PROP_FPS)), (w, h))
-
-    # Read until video is completed
-    while stream.isOpened():
-        # Capture frame-by-frame
-        success, frame = stream.read()
-        if success:
-            boxes = detector.detect(frame, (640, 640))
-            boxes = boxes.astype('int32')
-            for box in boxes:
-                x_min = box[0]
-                y_min = box[1]
-                x_max = box[2]
-                y_max = box[3]
-                box_w = x_max - x_min
-                box_h = y_max - y_min
-
-                # remove a part of top area for alignment, see paper for details
-                x_min -= int(box_w * (scale - 1) / 2)
-                y_min += int(box_h * (scale - 1) / 2)
-                x_max += int(box_w * (scale - 1) / 2)
-                y_max += int(box_h * (scale - 1) / 2)
-                x_min = max(x_min, 0)
-                y_min = max(y_min, 0)
-                x_max = min(x_max, w - 1)
-                y_max = min(y_max, h - 1)
-                box_w = x_max - x_min + 1
-                box_h = y_max - y_min + 1
-                image = frame[y_min:y_max, x_min:x_max, :]
-                image = cv2.resize(image, (args.input_size, args.input_size))
-                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                image = np.stack((image, image, image), -1)
-                image = image.astype("float32")
-                cv2.subtract(image, mean, image)  # inplace
-                cv2.multiply(image, 1 / std, image)  # inplace
-                image = image.transpose((2, 0, 1))
-                image = np.ascontiguousarray(image)
-                image = np.expand_dims(image, 0)
-
-                output, score = landmark(image)  # confidence score
-                for i in range(params['num_lms']):
-                    x = int(output[i * 2] * box_w)
-                    y = int(output[i * 2 + 1] * box_h)
-                    cv2.circle(frame, (x + x_min, y + y_min), 1, (0, 255, 0), 1)
-
-            cv2.imshow('IMAGE', frame)
-
-            writer.write(frame.astype('uint8'))
-
-            # Press Q on keyboard to  exit
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
-        # Break the loop
-        else:
-            break
-    # When everything done, release the video capture object
-    stream.release()
-    writer.release()
     # Closes all the frames
     cv2.destroyAllWindows()
 
