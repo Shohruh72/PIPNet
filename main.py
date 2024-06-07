e4b79d39fa574ce87372dacfa515b177bba8423f
Shohruh Rakhmatov <122860237+Shohruh72@users.noreply.github.com>
2024-06-07 17:38:04 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,73 +1,123 @@
 import argparse
-import copy
-import csv
 import os
-
 import cv2
-import tqdm
+import csv
 import yaml
+import copy
+import tqdm
 import torch
 import numpy as np
-import torch.nn as nn
-from torch.utils.data import DataLoader
-from torchvision import models
 
 from nets import nn
 from utils import util
+from timm import utils
 from utils.dataset import Dataset
+from torch.utils.data import DataLoader
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
 def train(args, params):
-    backbone = models.resnet50(True)
+    # Seed
+    util.setup_seed()
+    # Model
+    model = nn.PIPNet(params, args.resnet_type, *util.compute_indices(f'{args.data_dir}/indices.txt', params))
+    model.cuda()
 
-    model = nn.PIPNet(params, backbone, args.resnet_type,
-                      *util.compute_indices(f'{args.data_dir}/indices.txt', params)).to(device)
-    criterion = util.ComputeLoss(params)
-    optimizer = torch.optim.Adam(model.parameters(), lr=params['init_lr'])
-    scheduler = torch.optim.lr_scheduler.MultiStepLR(optimizer, milestones=params['decay_steps'], gamma=0.1)
+    # Optimizer
+    optimizer = torch.optim.RMSprop(model.parameters(), lr=params['init_lr'], weight_decay=1e-4)
+    # Scheduler
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=10, min_lr=1e-6)
+
+    # Dataset
     dataset = Dataset(params, f'{args.data_dir}/train.txt', True)
-    loader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True, num_workers=8, pin_memory=True,
-                        drop_last=True)
+
+    sampler = None
+    if args.distributed:
+        sampler = torch.utils.data.distributed.DistributedSampler(dataset)
+
+    loader = DataLoader(dataset, args.batch_size, not args.distributed,
+                        sampler, num_workers=8, pin_memory=True, drop_last=True)
+
+    if args.distributed:
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = torch.nn.parallel.DistributedDataParallel(module=model,
+                                                          device_ids=[args.local_rank])
+
+    # Start Training ...
     best = float('inf')
     num_batch = len(loader)
+    criterion = util.ComputeLoss(params)
+    amp_scale = torch.cuda.amp.GradScaler()
     with open('outputs/weights/logs.csv', 'w') as f:
-        writer = csv.DictWriter(f, fieldnames=['epoch', 'NME'])
-        writer.writeheader()
+        if args.local_rank == 0:
+            writer = csv.DictWriter(f, fieldnames=['epoch', 'NME'])
+            writer.writeheader()
+
         for epoch in range(args.epochs):
             model.train()
-
             p_bar = enumerate(loader)
-            print(('\n' + '%10s' * 2) % ('epoch', 'loss'))
-            p_bar = tqdm.tqdm(iterable=p_bar, total=num_batch)
+            avg_loss = util.AverageMeter()
+
+            if args.local_rank == 0:
+                print(('\n' + '%10s' * 2) % ('epoch', 'loss'))
+                p_bar = tqdm.tqdm(iterable=p_bar, total=num_batch)
 
             for i, (images, labels) in p_bar:
                 images = images.to(device)
-                loss = criterion(model(images), labels).to(device)
+                labels = [label.to(device) for label in labels]
                 optimizer.zero_grad()
-                loss.backward()
-                optimizer.step()
-                s = ('%10s' + '%10.4g') % (f'{epoch + 1}/{args.epochs}', loss.item())
-                p_bar.set_description(s)
 
-            scheduler.step()
+                with torch.cuda.amp.autocast():
+                    outputs = model(images)
+                    loss = criterion(outputs, labels)
 
-            last = test(args, params, model)
-            writer.writerow({'NME': str(f'{last:.3f}'),
-                             'epoch': str(epoch + 1).zfill(3)})
-            f.flush()
+                # Backward
+                amp_scale.scale(loss).backward()
 
-            if best > last:
-                best = last
+                # Optimize
+                amp_scale.step(optimizer)
+                amp_scale.update(None)
+                optimizer.zero_grad()
 
-            ckpt = {'model': copy.deepcopy(model).half()}
-            torch.save(ckpt, './outputs/weights/last.pt')
-            if best == last:
-                torch.save(ckpt, './outputs/weights/best.pt')
-            del ckpt
+                # Log
+                if args.distributed:
+                    loss = utils.reduce_tensor(loss.data, args.world_size)
 
-    # torch.cuda.empty_cache()
+                avg_loss.update(loss.item(), images.size(0))
+                if args.local_rank == 0:
+                    s = ('%10s' + '%10.4g') % (f'{epoch + 1}/{args.epochs}', loss.item())
+                    p_bar.set_description(s)
+
+            # Scheduler
+
+            if args.local_rank == 0:
+                # Start Testing ...
+                last = test(args, params, model)
+                scheduler.step(last)
+                writer.writerow({'NME': str(f'{last:.3f}'),
+                                 'epoch': str(epoch + 1).zfill(3)})
+                f.flush()
+
+                # Update best NME
+                if best > last:
+                    best = last
+
+                # Model Save
+                ckpt = {'model': copy.deepcopy(model).half()}
+
+                # Save last and best result
+                torch.save(ckpt, './outputs/weights/last.pt')
+                if best == last:
+                    torch.save(ckpt, './outputs/weights/best.pt')
+                del ckpt
+                print(f"Best NME = {best:.3f}")
+
+    if args.local_rank == 0:
+        util.strip_optimizer('./outputs/weights/best.pt')
+        util.strip_optimizer('./outputs/weights/last.pt')
+
+    torch.cuda.empty_cache()
 
 
 @torch.no_grad()
@@ -76,7 +126,7 @@ def test(args, params, model=None):
     loader = DataLoader(Dataset(params, f'{args.data_dir}/test.txt', False))
 
     if model is None:
-        model = torch.load(f'./outputs/weights/last.pt', map_location='cuda')['model'].float()
+        model = torch.load(f'./outputs/weights/best.pt', map_location='cuda')['model'].float()
 
     model.half()
     model.eval()
@@ -96,12 +146,9 @@ def test(args, params, model=None):
         norm = np.linalg.norm(a[index[0]] - b[index[1]])
         nme_merge.append(util.compute_nme(output, target, norm))
 
-    # Print results
     nme = np.mean(nme_merge) * 100
-    print('%20.3g' % nme)
-
-    # Return results
-    model.float()  # for training
+    print(f"Last NME = {nme:.3f}")
+    model.float()
     return nme
 
 
@@ -179,25 +226,30 @@ def demo(params):
             # Press Q on keyboard to  exit
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
-        # Break the loop
         else:
             break
-    # When everything done, release the video capture object
     stream.release()
-    # Closes all the frames
     cv2.destroyAllWindows()
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir', type=str, default='./Dataset/images')
-    parser.add_argument('--resnet_type', type=str, default='resnet50')
+    parser.add_argument('--data_dir', type=str, default='../../Datasets/LMK/300W/images')
+    parser.add_argument('--resnet_type', type=str, default='resnet18')
     parser.add_argument('--epochs', type=int, default=120)
+    parser.add_argument('--local_rank', default=0, type=int)
     parser.add_argument('--batch-size', type=int, default=16)
     parser.add_argument('--train', action='store_true')
-    parser.add_argument('--test', default=True, action='store_true')
+    parser.add_argument('--test', action='store_true')
     parser.add_argument('--demo', action='store_true')
     args = parser.parse_args()
+
+    args.world_size = int(os.getenv('WORLD_SIZE', 1))
+    args.distributed = int(os.getenv('WORLD_SIZE', 1)) > 1
+
+    if args.distributed:
+        torch.cuda.set_device(device=args.local_rank)
+        torch.distributed.init_process_group(backend='nccl', init_method='env://')
 
     with open(os.path.join('utils', 'cfg.yaml'), errors='ignore') as f:
         params = yaml.safe_load(f)
